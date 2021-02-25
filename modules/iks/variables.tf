49660142156c7a6a7c79a328a09814c001813c45
Michael Maurer <mimaurer@cisco.com>
2021-02-25 18:13:11 +0100

    added more variables

--- modules/iks/variables.tf
+++ modules/iks/variables.tf
@@ -20,3 +20,27 @@ variable "k8s_version" {
   type    = string
   default = "1.18.12"
 }
+
+#dns_servers
+
+#ntp_servers
+
+variable "timezone" {
+  type    = string
+  default = "Europe/Vienna"
+}
+
+variable "pod_network_cidr" {
+  type    = string
+  default = "172.16.0.0/17"
+}
+
+variable "service_cidr" {
+  type    = string
+  default = "172.16.128.0/17"
+}
+
+variable "proxy_enabled" {
+  type    = bool
+  default = false
+}
